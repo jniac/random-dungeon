0e85828ce63b289f18cb3351307d0fe2355921c9
jniac <joseph.merdrignac@gmail.com>
2018-10-31 16:34:43 +0100

    start

---
+++ src/grid.js
@@ -0,0 +1,110 @@
+
+
+class Neighbors extends Array {
+
+    get E() { return this[0] }
+    set E(value) { this[0] = value }
+    get SE() { return this[1] }
+    set SE(value) { this[1] = value }
+    get S() { return this[2] }
+    set S(value) { this[2] = value }
+    get SW() { return this[3] }
+    set SW(value) { this[3] = value }
+    get W() { return this[4] }
+    set W(value) { this[4] = value }
+    get NW() { return this[5] }
+    set NW(value) { this[5] = value }
+    get N() { return this[6] }
+    set N(value) { this[6] = value }
+    get NE() { return this[7] }
+    set NE(value) { this[7] = value }
+
+}
+
+class Cell {
+
+    constructor(x, y) {
+
+        let neighbors = new Neighbors()
+
+        Object.assign(this, { x, y, neighbors })
+
+    }
+
+}
+
+class Grid {
+
+    constructor(width = 40, height = 40) {
+
+        let cells = []
+
+        for (let y = 0; y < height; y++) {
+
+            for (let x = 0; x < width; x++) {
+
+                let cell = new Cell(x, y)
+
+                if (y > 0) {
+
+                    let N = cells[(y - 1) * width + x]
+                    cell.neighbors.N = N
+                    N.neighbors.S = cell
+
+                    if (x > 0) {
+
+                        let NW = cells[(y - 1) * width + x - 1]
+                        cell.neighbors.NW = NW
+                        NW.neighbors.SE = cell
+
+                    }
+
+                    if (x < width - 1) {
+
+                        let NE = cells[(y - 1) * width + x + 1]
+                        cell.neighbors.NE = NE
+                        NE.neighbors.SW = cell
+
+                    }
+
+                }
+
+                if (x > 0) {
+
+                    let W = cells[y * width + x - 1]
+                    cell.neighbors.W = W
+                    W.neighbors.E = cell
+
+                }
+
+                cells.push(cell)
+
+            }
+
+        }
+
+        Object.assign(this, { width, height, cells })
+
+    }
+
+    cell(x, y) {
+
+        let { cells, width, height } = this
+
+        if (x < 0)
+            x += width
+
+        if (y < 0)
+            y += height
+
+        return cells[y * width + x]
+
+    }
+
+}
+
+Object.assign(window, { Grid, Cell })
+
+let grid = new Grid(10, 10)
+
+Object.assign(window, { grid })
